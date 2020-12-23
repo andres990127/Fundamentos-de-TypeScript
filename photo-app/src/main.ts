8587c2392fe92a94926033ac7fdfd4804fbd63bb
Andrés Gutiérrez <andresmauricio0127@hotmail.com>
2020-12-23 09:57:10 -0500

    Ultimo commit

--- photo-app/src/main.ts
+++ photo-app/src/main.ts
@@ -1,11 +1,19 @@
-import { User, Album, Picture, PhotoOrientation } from './photo-app';
+// import {User, Album, Picture, PhotoOrientation} from './photo-app';
 
-const user = new User(1, 'Erickowski', 'Erick', true);
-const album = new Album(10, 'Platzi Album');
-const picture = new Picture(1, 'Foto', '2020-12', PhotoOrientation.Landscape);
+import { User } from "./user";
+import { Album } from "./album";
+import { Picture } from "./picture";
+import { PhotoOrientation } from "./photo-orientation";
 
+const user = new User(1, 'luixaviles', 'Luis', true);
+const album = new Album(10, 'Platzi Pictures');
+const picture = new Picture(1, 'TypeScript Course', '2020-03', PhotoOrientation.Landscape);
 // Creamos relaciones
 user.addAlbum(album);
 album.addPicture(picture);
-
 console.log('user', user);
+
+// Borramos Album
+user.removeAlbum(album);
+console.log('user', user);
+
